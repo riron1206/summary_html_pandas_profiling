1f8ea5b53636ed32cbbfa038edbe2e6fea1e915b
riron1206 <rironyokoi@gmail.com>
2020-12-04 19:25:02 +0900

    edit bat

--- summary_html_pandas_profiling.bat
+++ summary_html_pandas_profiling.bat
@@ -4,10 +4,12 @@ call activate tfgpu
 
 @rem set PY_DIR=C:\Users\81908\MyGitHub\summary_html_pandas_profiling\
 
+@rem call python %PY_DIR%summary_html_pandas_profiling.py ^
 call python summary_html_pandas_profiling.py ^
 -o tmp ^
 -i D:\work\kaggle_data\my_profile_dataset\807993_1384542_bundle_archive\tmp\lgb_tuning\tests\input\bike-sharing-demand\train.csv
 
+@rem call python %PY_DIR%summary_html_pandas_profiling.py ^
 call python summary_html_pandas_profiling.py ^
 -o tmp ^
 -i D:\work\kaggle_data\my_profile_dataset\807993_1384542_bundle_archive\tmp\lgb_tuning\tests\input\bike-sharing-demand\test.csv
